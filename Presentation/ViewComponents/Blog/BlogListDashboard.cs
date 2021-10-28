0c32514ac083cb6412a040cb482090130b29f6b0
Burak KALAYCI <torukobyte@gmail.com>
2021-10-28 22:14:56 +0300

    day 16 (lesson 62 to 65) completed successfully!

---
+++ Presentation/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        private readonly BlogManager _blogManager = new(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ Presentation/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        private readonly CategoryManager _categoryManager = new(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryManager.GetList();
+            return View(values);
+        }
+        
+    }
+}
